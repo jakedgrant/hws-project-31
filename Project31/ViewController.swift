b9f688929d54c442273611d50f2cf30638bd4b6b
jakedgrant <jake.d.grant@gmail.com>
2018-02-20 20:56:01 -0600

    Initial Commit

---
+++ Project31/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project31
+//
+//  Created by Jake Grant on 2/20/18.
+//  Copyright © 2018 Jake Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
